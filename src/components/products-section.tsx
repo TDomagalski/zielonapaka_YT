50c7917778a301d1c50661e41bbc113c0c22b8ed
TDomagalski <tomekwebdev@gmail.comL>
2025-03-21 12:20:01 +0100

    feat: add component product-section

---
+++ src/components/products-section.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+import CartIcon from "./icons/cartIcon";
+
+export default function ProductsSection() {
+  return (
+    <section className="max-w-screen-2xl mx-auto px-4 sm:px-8">
+      {/* Jest taka zasada że jak piszemy element section to zawsze pod nim musi się znaleść nagłówek. h1 może być użyty tylko raz na stronie więc trzeba użyć h2 */}
+      <h2 className="text-4xl font-medium tracking-tight text-gray-950 mb-8">
+        Świeżo, tanio i ekologicznie
+      </h2>
+      <div className="flex flex-col justify-between lg:flex-row lg:items-center mb-8 gap-4">
+        <p className="text-gray-700 text-base">Liczba produktów: 16</p>
+        <div>
+          <label
+            className="text-gray-950 font-medium text-sm mr-4"
+            htmlFor="sort"
+          >
+            Sortuj wg.
+          </label>
+          <select
+            className="text-grey-700 text-base border border-gray-200 p-1 rounded-lg shadow-lg shadow-gray-200/50"
+            name="sort"
+            id="sort"
+          >
+            <option value="default">Domyślnie</option>
+            <option value="priceDesc">Cena: od najwyższej</option>
+            <option value="priceAsc">Cena: od najniższej</option>
+          </select>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <Link
+          className="rounded-lg overflow-hidden border border-gray-200 shadow-lg shadow-gray-200/50 hover:translate-y-1 transition-all duration-300"
+          href=""
+        >
+          <figure>
+            <Image
+              className="w-full h-60 object-cover"
+              src="https://images.unsplash.com/photo-1582284540020-8acbe03f4924?q=80&w=4470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Pomidory"
+              width={0}
+              height={0}
+              unoptimized={true} // Wyłącza optymalizację Next.js
+            />
+            <div className="p-4">
+              <div className="flex justify-between items-center">
+                <p className="text-lg font-medium text-gray-950">Pomidory</p>
+                <CartIcon />
+              </div>
+              <p className="text-lg text-gray-700">15 zł / kg</p>
+            </div>
+          </figure>
+        </Link>
+      </div>
+    </section>
+  );
+}
